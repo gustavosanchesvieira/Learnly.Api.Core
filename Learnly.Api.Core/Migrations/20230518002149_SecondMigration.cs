e9a067f0a14771a6b16e5d1bf93ec0cc309f5327
Gustavo Sanches Vieira <78124205+gustavosanchesvieira@users.noreply.github.com>
2023-05-17 21:23:22 -0300

    [Gustavo] - Alterações no banco de dados, entidades de faltas e materias.

---
+++ Learnly.Api.Core/Migrations/20230518002149_SecondMigration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Learnly.Api.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AbcencesLimit",
+                table: "Abcenses");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AbcencesLimit",
+                table: "Subjects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AbcencesLimit",
+                table: "Subjects");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AbcencesLimit",
+                table: "Abcenses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
